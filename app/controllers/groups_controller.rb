4fb3d1c865979b0406cb0f7d588ad2c8c9b578bf
EliudDmx <eliud.dmx@gmail.com>
2015-08-22 02:18:02 -0500

    Add specs for group model

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,58 +1,39 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:show, :edit, :update, :destroy]
+ # before_action :set_group, only: [:show, :edit, :update, :destroy]
 
   # GET /groups
   def index
-    @groups = Group.all
+    render json:  Group.all
   end
 
-  # GET /groups/1
-  def show
-  end
-
-  # GET /groups/new
-  def new
-    @group = Group.new
-  end
-
-  # GET /groups/1/edit
-  def edit
-  end
-
-  # POST /groups
   def create
-    @group = Group.new(group_params)
-
-    if @group.save
-      redirect_to @group, notice: 'Group was successfully created.'
-    else
-      render :new
-    end
+	group = Group.new(group_params)
+		if group.save 
+			render json: group, status: :created
+		else
+			render json: {errors: group.errors}, status: 422
+		end
   end
 
-  # PATCH/PUT /groups/1
   def update
-    if @group.update(group_params)
-      redirect_to @group, notice: 'Group was successfully updated.'
-    else
-      render :edit
-    end
+	group = Group.find(params[:id])
+
+		if group.update(group_params)
+			render json: group, status: :ok
+		end 	
+
   end
 
-  # DELETE /groups/1
   def destroy
-    @group.destroy
-    redirect_to groups_url, notice: 'Group was successfully destroyed.'
-  end
+  		
+	Group.find(params[:id]).destroy
+		head :no_content
+ end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_group
-      @group = Group.find(params[:id])
-    end
+ private
+  	def group_params
+		params.require(:group).permit(:name, :day, :time)
+	end
 
-    # Only allow a trusted parameter "white list" through.
-    def group_params
-      params.require(:group).permit(:name, :day, :time)
-    end
+
 end
